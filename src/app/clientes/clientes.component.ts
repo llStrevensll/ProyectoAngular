c184b5566dc88f0438e14b538c2899d6005e33dd
Steven <anguelr12@hotmail.com>
2020-04-24 09:52:20 -0500

    creado archivo cliente.json.ts con la lista de objetos cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,20 +9,12 @@ import { Cliente } from './cliente';
 export class ClientesComponent implements OnInit {
 
   // Lista de tipo cliente
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Angel', apellido: 'Rodriguez', email: 'correo@email.com', createAt: '2020-12-11'},
-    {id: 2, nombre: 'Emanuel', apellido: 'Rodriguez', email: 'correo1@email.com', createAt: '2020-12-11'},
-    {id: 3, nombre: 'Steven', apellido: 'Castro', email: 'correo2@email.com', createAt: '2020-12-11'},
-    {id: 4, nombre: 'Nombre', apellido: 'Apellido', email: 'correo3@email.com', createAt: '2020-12-11'},
-    {id: 5, nombre: 'Nombre', apellido: 'Apellido', email: 'correo4@email.com', createAt: '2020-12-11'},
-    {id: 6, nombre: 'Nombre', apellido: 'Apellido', email: 'correo5@email.com', createAt: '2020-12-11'},
-    {id: 7, nombre: 'Nombre', apellido: 'Apellido', email: 'correo6@email.com', createAt: '2020-12-11'},
-
-  ];
+  clientes: Cliente[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+  // Lista de tipo cliente
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Angel', apellido: 'Rodriguez', email: 'correo@email.com', createAt: '2020-12-11'},
+    {id: 2, nombre: 'Emanuel', apellido: 'Rodriguez', email: 'correo1@email.com', createAt: '2020-12-11'},
+    {id: 3, nombre: 'Steven', apellido: 'Castro', email: 'correo2@email.com', createAt: '2020-12-11'},
+    {id: 4, nombre: 'Nombre', apellido: 'Apellido', email: 'correo3@email.com', createAt: '2020-12-11'},
+    {id: 5, nombre: 'Nombre', apellido: 'Apellido', email: 'correo4@email.com', createAt: '2020-12-11'},
+    {id: 6, nombre: 'Nombre', apellido: 'Apellido', email: 'correo5@email.com', createAt: '2020-12-11'},
+    {id: 7, nombre: 'Nombre', apellido: 'Apellido', email: 'correo6@email.com', createAt: '2020-12-11'}
+];
