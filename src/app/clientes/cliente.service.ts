e707003c9335643602963f69f32f7394deae5a74
Steven <anguelr12@hotmail.com>
2020-04-24 19:28:24 -0500

    conectar con el restservice con httpclient

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import {Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES); // Creamos nuestro flujo Observable a partir de los objetos CLIENTES
+    // return of(CLIENTES); // Creamos nuestro flujo Observable a partir de los objetos CLIENTES
+    return this.http.get<Cliente[]>(this.urlEndPoint); // Castear a <Cliente[]>
+/*
+      return this.http.get(this.urlEndPoint).pipe(
+        map(response => response as Cliente[]) // castear response a Cliente[]
+      );*/
   }
 }
